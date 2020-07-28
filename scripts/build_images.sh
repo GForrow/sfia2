61d27b16b3a9f9670b2ec162e9e7c711ce0e4820
GForrow <garyforrowbsc@gmail.com>
2020-07-28 10:30:29 +0000

    correcting build_images.sh

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -3,17 +3,17 @@
 # build service images if they do not exist locally
 
 if [[ "$(docker images -q forrow/service_1:latest 2> /dev/null)" == "" ]]; then
-    docker build -t forrow/service_1 ../Service_1
+    docker build -t forrow/service_1 ./Service_1
 fi
 
 if [[ "$(docker images -q forrow/service_2:latest 2> /dev/null)" == "" ]]; then
-    docker build -t forrow/service_2 ../Service_2
+    docker build -t forrow/service_2 ./Service_2
 fi
 
 if [[ "$(docker images -q forrow/service_1:latest 3> /dev/null)" == "" ]]; then
-    docker build -t forrow/service_3 ../Service_3
+    docker build -t forrow/service_3 ./Service_3
 fi
 
 if [[ "$(docker images -q forrow/service_4:latest 2> /dev/null)" == "" ]]; then
-    docker build -t forrow/service_4 ../Service_4
+    docker build -t forrow/service_4 ./Service_4
 fi
