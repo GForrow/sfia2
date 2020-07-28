bc0d153005337e5acd3169a48980844a6f458528
GForrow <garyforrowbsc@gmail.com>
2020-07-28 13:38:06 +0000

    Rolling update

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -5,19 +5,27 @@
 if [[ "$(docker images -q forrow/service_1:latest 2> /dev/null)" == "" ]]; then
     docker build -t forrow/service_1 ./Service_1
     docker push forrow/service_1:latest
+else
+    docker pull forrow/service_1:latest
 fi
 
 if [[ "$(docker images -q forrow/service_2:latest 2> /dev/null)" == "" ]]; then
     docker build -t forrow/service_2 ./Service_2
     docker push forrow/service_2:latest
+else
+    docker pull forrow/service_2:latest
 fi
 
 if [[ "$(docker images -q forrow/service_3:latest 2> /dev/null)" == "" ]]; then
     docker build -t forrow/service_3 ./Service_3
     docker push forrow/service_3:latest
+else
+    docker pull forrow/service_3:latest
 fi
 
 if [[ "$(docker images -q forrow/service_4:latest 2> /dev/null)" == "" ]]; then
     docker build -t forrow/service_4 ./Service_4
     docker push forrow/service_4:latest
+else
+    docker pull forrow/service_4:latest
 fi
---
+++ scripts/update_stack.sh
@@ -0,0 +1,15 @@
+if [[ "$(docker stack ls)" == *"lootstack"* ]]; then
+    echo "Lootstack is running."
+
+    echo "Updating service_1."
+    docker service update --image forrow/service_1 lootstack_service_1
+
+    echo "Updating service_2."
+    docker service update --image forrow/service_2 lootstack_service_2
+
+    echo "Updating service_3."
+    docker service update --image forrow/service_3 lootstack_service_3
+
+    echo "Updating service_4."
+    docker service update --image forrow/service_4 lootstack_service_4
+fi
