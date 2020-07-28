f1985e3c432b3563e94d8d086b6e20c3c6d28e9a
GForrow <garyforrowbsc@gmail.com>
2020-07-28 11:36:28 +0000

    trying rolling update

--- scripts/build_stack.sh
+++ scripts/build_stack.sh
@@ -2,12 +2,23 @@
 
 #build stack for running services
 
-docker stack deploy --compose-file docker-compose.yml lootstack
-
 if [[ "$(docker stack services lootstack 2> /dev/null)" == "" ]]; then
     docker stack deploy --compose-file docker-compose.yml lootstack
+    echo "Lootstack initialised."
 fi
 
 if [[ "$(docker stack ls)" == *"lootstack"* ]]; then
-    echo "Lootstack is running"
+    echo "Lootstack is running."
+
+    echo "Updating service_1."
+    docker service update forrow/service_1
+
+    echo "Updating service_2."
+    docker service update forrow/service_2
+
+    echo "Updating service_3."
+    docker service update forrow/service_3
+
+    echo "Updating service_4."
+    docker service update forrow/service_4
 fi
