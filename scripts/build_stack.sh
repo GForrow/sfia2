01bda37fab3d7de8ff2a3d433ddba5cba8854151
GForrow <garyforrowbsc@gmail.com>
2020-07-28 13:43:52 +0000

    Rolling update

--- scripts/build_stack.sh
+++ scripts/build_stack.sh
@@ -7,18 +7,3 @@ if [[ "$(docker stack services lootstack 2> /dev/null)" == "" ]]; then
     echo "Lootstack initialised."
 fi
 
-if [[ "$(docker stack ls)" == *"lootstack"* ]]; then
-    echo "Lootstack is running."
-
-    echo "Updating service_1."
-    docker service update --image forrow/service_1 lootstack_service_1
-
-    echo "Updating service_2."
-    docker service update lootstack_service_2
-
-    echo "Updating service_3."
-    docker service update lootstack_service_3
-
-    echo "Updating service_4."
-    docker service update lootstack_service_4
-fi
