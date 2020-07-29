2a06d512ec90d1b6f33b238e52604a64c8a1cfb3
GForrow <garyforrowbsc@gmail.com>
2020-07-29 13:34:32 +0000

    Commented out db code from / route to test connection first.

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -41,9 +41,9 @@ def home():
     response = requests.get('http://service_4:5003/randomword')
     print(response)
     sentence = response.text
-    loot_data = loot_table(
-        loot_result=sentence
-    )
-    db.session.add(loot_data)
-    db.session.commit()
+    # loot_data = loot_table(
+    #     loot_result=sentence
+    # )
+    # db.session.add(loot_data)
+    # db.session.commit()
     return render_template('index.html', sentence = sentence, title = 'Home')
