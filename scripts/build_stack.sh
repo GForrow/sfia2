b30d9a4295dc2f85b64881f4af2425052407e7e4
GForrow <garyforrowbsc@gmail.com>
2020-07-29 08:28:06 +0000

    Added some debug info

--- scripts/build_stack.sh
+++ scripts/build_stack.sh
@@ -5,5 +5,7 @@
 if [[ "$(docker stack services lootstack 2> /dev/null)" == "" ]]; then
     docker stack deploy --compose-file docker-compose.yml lootstack
     echo "Lootstack initialised."
+else
+    echo "lootstack already exists"
 fi
 
