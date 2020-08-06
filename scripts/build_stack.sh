32adb2e92f45d2608014e385ddb1d6fce166a3a5
GForrow <garyforrowbsc@gmail.com>
2020-08-06 09:38:34 +0000

    tidy files

--- scripts/build_stack.sh
+++ scripts/build_stack.sh
@@ -3,6 +3,8 @@
 #build stack for running services
 source /var/lib/jenkins/.bashenv
 
+ansible-playbook -i inventory.cfg playbook.yml
+
 if [[ "$(docker stack services lootstack 2> /dev/null)" == "" ]]; then
     docker stack deploy --compose-file docker-compose.yml lootstack
     echo "=================lootstack initialised================="
